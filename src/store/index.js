365058fed8044e4103dc65a5fbacd198ed2a3122
Manuela-Valencia <valenciamanuela10@gmail.com>
2023-06-30 09:34:48 -0500

    corrigiendo

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsG from './slices/Products.slice'
+import productsG from './slices/products.slice';
 import cart from './slices/cart.slice'
 export default configureStore({
     reducer: {
